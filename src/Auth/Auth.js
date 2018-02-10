db9b84c46095442b7ebefc57f02c19c95411d8fc
Spencer Hawk <spencerjhawk@gmail.com>
2018-02-10 11:18:38 -0600

    modified Auth.js

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -26,5 +26,45 @@ constructor() {
     this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
+handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.replace('/home');
+      } else if (err) {
+        history.replace('/home');
+        console.log(err);
+      }
+    });
+  }
+
+setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+logout() {
+    // Clear Access Token and ID Token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+isAuthenticated() {
+    // Check whether the current time is past the 
+    // Access Token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
+
+
 
 
