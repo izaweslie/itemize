764cf65cb9a0eca2f6d7d4d09d063d5d3fcf403a
Spencer Hawk <spencerjhawk@gmail.com>
2018-02-12 19:52:59 -0600

    more crap

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -4,9 +4,9 @@ import auth0 from 'auth0-js';
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: 'https://warm-waters-37423.herokuapp.com/',
+    domain: 'https://warm-waters-37423.herokuapp.com/#',
     clientID: 'cYkWhCZ4ZSMAzoU4Kj3kWn8wv72gUKdN',
-    redirectUri: 'https://warm-waters-37423.herokuapp.com/',
+    redirectUri: 'https://warm-waters-37423.herokuapp.com/#',
     audience: 'https://itemizeteam.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid'
